2abda5d1ec4e74a999510a103b0763e13b038046
A.S. Lomoff <lomov.as@gmail.com>
2014-03-11 14:25:49 +0300

    add 3rd task

--- solutions/allomov/1/sources/1_3/main.cpp
+++ solutions/allomov/1/sources/1_3/main.cpp
@@ -1,15 +1,149 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stack>
+#include <algorithm>
+#include <vector>
+#include <list>
 
-int main( int argc, char* argv[] )
+struct Point {
+  int x, y;
+  Point(unsigned int x_, unsigned int y_): x(x_), y(y_) {}
+};
+
+template <typename T>
+class BoundsMatrix
 {
-  std::ifstream input_file( SOURCE_DIR "/CMakeLists.txt" );
-  std::string line;
-  while( !input_file.eof() )
-  {
-    std::getline( input_file, line );
-    std::cout << line.c_str() << std::endl;
+  std::vector<T> inner_;
+  unsigned int dimx_, dimy_;
+
+public:
+
+  void setDimensions (unsigned int dimx, unsigned int dimy) {
+    dimx_ = dimx; dimy_ = dimy;
+    inner_.resize (dimx_ * dimy_);
   }
-  input_file.close();
+
+  T& operator()(unsigned int x, unsigned int y) {
+//    if (x >= dimx_ || y>= dimy_) throw 0; // ouch
+    return inner_[dimx_ * y + x];
+  }
+
+  void set(unsigned int x, unsigned int y, T value) {
+    inner_[dimx_ * y + x] = value;
+  }
+
+  unsigned int dimx() {
+    return dimx_;
+  }
+
+  unsigned int dimy() {
+    return dimy_;
+  }
+};
+
+
+class IslandsMap {
+private:
+  BoundsMatrix<short> matrix;
+
+public: 
+  void readFromFile(std::ifstream& input_file) {
+    std::vector<std::string> lines;
+    std::string line;
+    while(!input_file.eof()) { 
+      std::getline(input_file, line);
+      lines.push_back(line);
+    }
+    matrix.setDimensions(line.length(), lines.size());
+    for (int i = 0; i < lines.size(); i++) {
+      line = lines.at(i);
+      for (int j = 0; j < line.length(); j++) {
+        short value = (line.at(j) == 'o' ? 1 : 0);
+        matrix.set(j, i, value);
+      }
+    }
+
+  }
+  
+  bool isLand(int x, int y) {
+    if (x < 0 || y < 0 || x >= dimx() || y >= dimy()) return false;
+    return matrix(x, y) != 0;
+  }
+
+  bool isMarked(unsigned int x, unsigned int y) {
+    return matrix(x, y) > 1;
+  }
+
+  void markLand(unsigned int x, unsigned int y) {
+    matrix.set(x, y, 100);
+  }
+
+  unsigned int dimx() {
+    return matrix.dimx();
+  }
+
+  unsigned int dimy() {
+    return matrix.dimy();
+  }
+
+  std::list<Point> landsNearby(int x, int y) {
+    std::list<Point> result;
+    if (isLand(x+1, y)) result.push_back(Point(x+1, y));
+    if (isLand(x-1, y)) result.push_back(Point(x-1, y));
+    if (isLand(x, y+1)) result.push_back(Point(x, y+1));
+    if (isLand(x, y-1)) result.push_back(Point(x, y-1));
+    return result;
+  }
+    
+  void out(){
+    for (int y = 0; y < matrix.dimy(); y++) {
+        for (int x = 0; x < matrix.dimx(); x++) {
+            std::cout << matrix(x, y);
+        }
+        std::cout << std::endl;
+    }
+
+  }
+
+};
+
+
+int main(int argc, char* argv[]) {    
+
+    std::ifstream input_file(SOURCE_DIR "/input.txt");
+    std::ofstream output_file(SOURCE_DIR "/output.txt");
+    IslandsMap map;
+    map.readFromFile(input_file);
+    map.out();
+    unsigned int iselands_count = 0;
+    std::stack<Point> stack;
+
+    for (int y = 0; y < map.dimy(); y++) {
+      for (int x = 0; x < map.dimx(); x++) {
+        if (map.isLand(x, y) && !map.isMarked(x, y)) {
+          std::cout << "Found an island (" << x << ", " << y << std::endl;
+          stack.push(Point(x, y));
+          iselands_count++;
+          while (!stack.empty()) {
+            Point current_point = stack.top();
+            stack.pop();
+            map.markLand(current_point.x, current_point.y);
+            std::cout << "Marked island (" << current_point.x << ", " << current_point.y << std::endl;
+            std::list<Point> neighbors = map.landsNearby(current_point.x, current_point.y);
+            for(std::list<Point>::iterator iterator = neighbors.begin(); iterator != neighbors.end(); iterator++) {
+              Point neighbor = *iterator;
+              if (!map.isMarked(neighbor.x, neighbor.y)) {
+                std::cout << "Neigbor added (" << neighbor.x << ", " << neighbor.y << std::endl;
+                stack.push(neighbor);
+              }
+            }
+          }
+        }
+      }
+    }
+
+    output_file << iselands_count << std::endl;
+    input_file.close();
+    output_file.close();
 }
